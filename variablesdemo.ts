268267cb7f7e968124263eae990a54645b7409fd
eswaribala <Parameswaribala@gmail.com>
2018-03-26 13:03:26 +0530

    Add files via upload

---
+++ variablesdemo.ts
@@ -0,0 +1,15 @@
+var firstName:string="Parameswari";
+var lastName:string="Bala";
+var age:number=47;
+var date:Date=new Date("1970/2/12");
+var maritalStatus:boolean=true;
+console.log("FirstName=",firstName);
+
+//array 
+var skillSet:string[]=["C++","Java","Angular","HTML5"];
+skillSet.forEach(obj=>{
+    console.log(obj);
+})
+
+
+
