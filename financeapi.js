433c2076d698e7be49ad55ec5b185d462872fe7d
eswaribala <Parameswaribala@gmail.com>
2018-03-28 15:16:21 +0530

    Add files via upload

--- financeapi.js
+++ financeapi.js
@@ -21,6 +21,22 @@ app.get("/stockquotes", function(request, response) {
     response.send(stocks);
 });
 
+app.post('/newaccount',function(request,response)
+{
+    console.log("Testing...")
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    response.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    response.setHeader('Access-Control-Allow-Credentials', true);
+    console.dir(request.query);
+    //console.log(request);
+    refAddState.Add(request.query.ID,request.query.Name);
+
+    response.end("New Account information received successfully");
+
+})
+
+
 app.listen(3000, function(){
     console.log('The server is running, ' +
         ' please open your browser at http://localhost:3000',
